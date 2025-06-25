e6bd6e7f80268d2c157d15e195dd168d378cdb2c
rodneyberubee <rodney@vivaitable.com>
2025-06-25 07:23:12 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import ReservationPage from './pages/ReservationPage.jsx';
+import UpdatePage from './pages/UpdatePage.jsx';
+import CancelPage from './pages/CancelPage.jsx';
+import CheckAvailabilityPage from './pages/CheckAvailabilityPage.jsx';
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Viv Reservation System</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">New Reservation</Link></li>
+            <li><Link to="/update">Update Reservation</Link></li>
+            <li><Link to="/cancel">Cancel Reservation</Link></li>
+            <li><Link to="/check">Check Availability</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<ReservationPage />} />
+          <Route path="/update" element={<UpdatePage />} />
+          <Route path="/cancel" element={<CancelPage />} />
+          <Route path="/check" element={<CheckAvailabilityPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
